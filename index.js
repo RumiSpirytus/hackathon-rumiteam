674f931fb87b1ba03a642ec983e81412f9fe906a
PhamMinhVuong <100543300+PhamMinhVuong21020425@users.noreply.github.com>
2024-01-12 00:08:53 +0700

    Fix bug

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 const express = require('express')
 const cors = require('cors')
-const app = express()
 require('dotenv').config();
-app.use(cors())
-app.use(express.json());
 
+const port = 3000
+const app = express()
+
+app.use(cors())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
 const endpoint = process.env.AZURE_OPENAI_ENDPOINT;
 const azureApiKey = process.env.AZURE_OPENAI_API_KEY;
+const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
+const deploymentId = process.env.MODEL;
 
-const messages = [
+const messages1 = [
     { role: "user", content: "Summary this Document for me !" },
-    { role: "user", content: "Generate 10 questions based on this document" },
 ];
 
-app.get('/generate-questions', async (req, res) => {
-    const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
-    const deploymentId = process.env.MODEL;
-    const result = await client.getChatCompletions(deploymentId, messages[0]);
+const messages2 = [
+    { role: "user", content: "Generate 10 questions based on this document" },
+]
 
-    res.json(result.choices[0])
+app.get('/generate-questions', async (req, res) => {
+    const result = await client.getChatCompletions(deploymentId, messages1);
+    res.json(result.choices[0].message.content)
 })
 
 app.get('/summary-document', async (req, res) => {
-    const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
-    const deploymentId = process.env.MODEL;
-    const result = await client.getChatCompletions(deploymentId, messages[1]);
-
-    res.json(result.choices[0])
+    const result = await client.getChatCompletions(deploymentId, messages2);
+    res.json(result.choices[0].message.content)
 })
-app.listen(3000)
 
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
