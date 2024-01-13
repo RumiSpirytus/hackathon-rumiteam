ef48f3f620f369dd125e916d6c7eaeb8b3c71038
NguyenDucThien <nguyenducthienfu@gmail.com>
2024-01-14 01:16:53 +0700

    extensions done

--- tool/mainTool.js
+++ tool/mainTool.js
@@ -1,20 +1,28 @@
 // document.getElementById("generateQuestions").addEventListener("click", generateQuestions);
 // document.getElementById("summaryDocument").addEventListener("click", summaryDocument);
+function general(link) {
+    const loadingIndicator = document.getElementById('loadingIndicator');
+    loadingIndicator.style.display = 'block';
+    const file = inputFile.files[0]
+    const formData = new FormData()
+    formData.append("pdfFile", file)
+
+    fetch(link, {
+        method: "POST",
+        body: formData
+    }).then(response => response.json())
+        .then(summaryText => {
+            resultText.value = summaryText.trim()
+            loadingIndicator.style.display = 'none';
+        })
+}
+
 
 const btn1 = document.getElementById("summaryDocument");
 btn1.addEventListener("click", function () {
     btn1.disabled = true;
     chrome.tabs.query({ currentWindow: true, active: true }, function () {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://localhost:3000/summary-document", true);
-        xhr.onload = function () {
-            var text = xhr.responseText;
-            const summaryDocument = document.getElementById("page");
-            summaryDocument.innerHTML = "<h2>Summary:</h2>" + text.replaceAll('\\n', '<br><br>');
-            btn1.disabled = false;
-            btn1.innerHTML = "Summary Document";
-        }
-        xhr.send();
+        general("https://hackathon-rumiteam.vercel.app/summary-document")
     });
 });
 
@@ -22,16 +30,12 @@ const btn2 = document.getElementById("generateQuestions");
 btn2.addEventListener("click", function () {
     btn2.disabled = true;
     chrome.tabs.query({ currentWindow: true, active: true }, function () {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://localhost:3000/generate-questions", true);
-        xhr.onload = function () {
-            var text = xhr.responseText;
-            const summaryDocument = document.getElementById("page");
-            summaryDocument.innerHTML = "<h2>Questions:</h2>" + text.replaceAll('\\n', '<br><br>');
-            btn2.disabled = false;
-            btn2.innerHTML = "Generate Questions";
-        }
-        xhr.send();
+        general("https://hackathon-rumiteam.vercel.app/generate-questions")
     });
 });
+
+const inputFile = document.getElementById("inputFile")
+const btnUpload = document.getElementById("btnUpload")
+const resultText = document.getElementById("resultText")
+
 
